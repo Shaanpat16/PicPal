3fc4e087c42a662de09b7c97812e040e9c951659
Shaanpat16 <shaanpat16@gmail.com>
2025-05-15 22:09:02 -0400

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  media: String,
+  caption: String,
+  hashtags: [String],
+  likes: { type: Number, default: 0 },
+  likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    text: String,
+    createdAt: { type: Date, default: Date.now }
+  }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Post', postSchema);
 
