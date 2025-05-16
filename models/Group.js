006f86defd637efb389e5a54f1184309843571c4
Shaanpat16 <shaanpat16@gmail.com>
2025-05-15 22:09:13 -0400

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  name: String,
+  isPrivate: Boolean,
+  joinCode: String,
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
+});
+
+module.exports = mongoose.model('Group', groupSchema);
 
