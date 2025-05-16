7bfed2669d786be6b91b6227c7a33c33d3cba9ce
Shaanpat16 <shaanpat16@gmail.com>
2025-05-15 22:09:41 -0400

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+const User = require('../models/User');
+
+router.get('/', async (req, res) => {
+  const posts = await Post.find().sort({ createdAt: -1 }).populate('user', 'username profilePic');
+  res.json(posts);
+});
+
+router.post('/', async (req, res) => {
+  const { userId, media, caption, hashtags } = req.body;
+  const newPost = new Post({ user: userId, media, caption, hashtags });
+  await newPost.save();
+  res.status(201).json(newPost);
+});
+
+module.exports = router;
 
