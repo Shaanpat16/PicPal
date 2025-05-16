285397ddaa996bfc15b44a69c838a849bdf426c8
Shaanpat16 <shaanpat16@gmail.com>
2025-05-15 22:08:53 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  googleId: String,
+  username: { type: String, required: true },
+  email: String,
+  password: String,
+  bio: { type: String, default: "" },
+  profilePic: String,
+  private: { type: Boolean, default: false },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  theme: { type: String, default: 'light' }
+});
+
+module.exports = mongoose.model('User', userSchema);
 
