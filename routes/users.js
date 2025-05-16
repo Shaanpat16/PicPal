76f3d5147bf18903b8a89e6b15d748c5c8f81a6a
Shaanpat16 <shaanpat16@gmail.com>
2025-05-15 22:09:50 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findById(req.params.id).populate('followers following', 'username profilePic');
+  res.json(user);
+});
+
+router.post('/update-bio', async (req, res) => {
+  const { userId, bio } = req.body;
+  await User.findByIdAndUpdate(userId, { bio });
+  res.sendStatus(200);
+});
+
+module.exports = router;
 
