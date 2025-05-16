4975b8b86fe2b4aba37cb7dfa39b63d669a669a4
Shaanpat16 <shaanpat16@gmail.com>
2025-05-15 22:09:59 -0400

    Update groups.js

--- routes/groups.js
+++ routes/groups.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Group = require('../models/Group');
+
+router.post('/create', async (req, res) => {
+  const { name, isPrivate } = req.body;
+  const joinCode = Math.random().toString(36).substring(2, 8).toUpperCase();
+  const group = new Group({ name, isPrivate, joinCode });
+  await group.save();
+  res.status(201).json(group);
+});
+
+module.exports = router;
 
